ba8d9a5ecd18f2023c585aa87521fad81a28c37d
Sam Selikoff <sam.selikoff@gmail.com>
2022-06-22 11:34:53 -0400

    Use wendler formula

--- app/utils/estimated-max.js
+++ app/utils/estimated-max.js
@@ -1,7 +1,7 @@
 export default function estimatedMax(set) {
   // Jim Wendler formula
-  // return +set.weight * +set.reps * 0.0333 + +set.weight;
+  return +set.weight * +set.reps * 0.0333 + +set.weight;
 
   // Brzycki formula
-  return set.weight / (1.0278 - 0.0278 * set.reps);
+  // return set.weight / (1.0278 - 0.0278 * set.reps);
 }
