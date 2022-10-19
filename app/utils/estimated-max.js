781638e0f91ec552131ebcd0456111322e0bc1f8
Sam Selikoff <sam.selikoff@gmail.com>
2022-10-19 19:15:46 -0400

    Wathan formula

--- app/utils/estimated-max.js
+++ app/utils/estimated-max.js
@@ -1,7 +1,15 @@
 export default function estimatedMax(set) {
   // Jim Wendler formula
-  return +set.weight * +set.reps * 0.0333 + +set.weight;
+  // return +set.weight * +set.reps * 0.0333 + +set.weight;
 
   // Brzycki formula
   // return set.weight / (1.0278 - 0.0278 * set.reps);
+
+  // Wathan formula
+  return (
+    +set.weight +
+    [...Array(set.reps - 1).keys()].reduce((prev, i) => {
+      return prev + +set.weight / (20 * ((i + 1) ^ 0.35));
+    }, 0)
+  );
 }
