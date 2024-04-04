b4d739bf2ca68f5dd18669b1890bec9c068a790f
Sam Selikoff <sam.selikoff@gmail.com>
2024-04-04 19:08:07 -0400

    wip

--- app/routes/__exercises/index.jsx
+++ app/routes/__exercises/index.jsx
@@ -7,8 +7,9 @@ import { requireUserId } from "~/session.server";
 import timeAgo from "~/utils/time-ago";
 import { AnimatePresence, motion } from "framer-motion";
 import pluralize from "pluralize";
+import { Box, Flex, Heading, Separator, Text } from "@radix-ui/themes";
 
-export async function loader({ request, params }) {
+export async function loader({ request }) {
   let userId = await requireUserId(request);
 
   let entries = await prisma.entry.findMany({
@@ -28,21 +29,21 @@ export default function ExercisesIndexPage() {
   let { entries } = useLoaderData();
 
   return (
-    <div className="my-6 px-4 pb-safe-bottom">
+    <Box px="4" my="5" className="pb-safe-bottom">
       {entries.length > 0 ? (
         <>
-          <h1 className="text-3xl font-bold">Latest exercises</h1>
+          <Heading size="7">Latest exercises</Heading>
 
-          <div className="mt-4 divide-y">
+          <Flex mt="6" direction="column" gap="4">
             {entries.map((entry) => (
-              <EntryCard entry={entry} key={entry.id} />
+              <EntryCard entry={entry} key={entry.i} />
             ))}
-          </div>
+          </Flex>
         </>
       ) : (
         <p className="mt-6">Choose an exercise.</p>
       )}
-    </div>
+    </Box>
   );
 }
 
@@ -50,90 +51,82 @@ function EntryCard({ entry }) {
   let [expanded, setExpanded] = useState(false);
 
   return (
-    <div>
-      <button
-        onClick={() => setExpanded(!expanded)}
-        className="relative w-full py-3 text-left"
-      >
-        <div>
-          <p className="text-[15px]">
-            <span className="text-lg font-semibold">{entry.exercise.name}</span>
-            <span className="px-1 font-medium text-gray-500">&middot;</span>
-            <span className="text-sm text-gray-500">{timeAgo(entry.date)}</span>
-          </p>
-          <motion.div layout="position" className="overflow-hidden">
-            <AnimatePresence initial={false}>
-              {entry.sets
-                .filter((set) => (!expanded ? set.tracked : true))
-                .map((set) => (
-                  <motion.div
-                    layout="position"
-                    variants={{
-                      hidden: { height: 0 },
-                      visible: { height: "auto" },
-                    }}
-                    initial="hidden"
-                    animate="visible"
-                    exit="hidden"
-                    key={set.id}
-                  >
-                    <motion.p
-                      variants={{
-                        hidden: {
-                          opacity: 0,
-                          transition: { type: "spring", duration: 0.35 },
-                        },
-                        visible: { opacity: 1 },
-                      }}
-                    >
-                      <span>
-                        {set.weight} lbs – {pluralize("rep", set.reps, true)}
-                      </span>
-                      <AnimatePresence>
-                        {expanded && set.tracked && (
-                          <motion.span
-                            key={set.id}
-                            initial={{ opacity: 0 }}
-                            exit={{
-                              opacity: 0,
-                              transition: { type: "spring", duration: 0.4 },
-                            }}
-                            animate={{ opacity: 1 }}
-                          >
-                            {" "}
-                            👈
-                          </motion.span>
-                        )}
-                      </AnimatePresence>
-                    </motion.p>
-                  </motion.div>
-                ))}
-              {expanded && entry.notes && (
-                <motion.div
-                  layout="position"
-                  initial={{ opacity: 0, height: 0 }}
-                  animate={{ opacity: 1, height: "auto" }}
-                  exit={{
-                    opacity: 0,
-                    height: 0,
-                    transition: { type: "spring", duration: 0.4 },
+    <>
+      <Flex justify="between" align="center">
+        <Text weight="bold" size="4">
+          {entry.exercise.name}
+        </Text>
+        <Text size="1" color="gray">
+          {timeAgo(entry.date)}
+        </Text>
+      </Flex>
+
+      <Separator size="4" />
+
+      {/* <motion.div layout="position" className="overflow-hidden">
+        <AnimatePresence initial={false}>
+          {entry.sets
+            .filter((set) => (!expanded ? set.tracked : true))
+            .map((set) => (
+              <motion.div
+                layout="position"
+                variants={{
+                  hidden: { height: 0 },
+                  visible: { height: "auto" },
+                }}
+                initial="hidden"
+                animate="visible"
+                exit="hidden"
+                key={set.id}
+              >
+                <motion.p
+                  variants={{
+                    hidden: {
+                      opacity: 0,
+                      transition: { type: "spring", duration: 0.35 },
+                    },
+                    visible: { opacity: 1 },
                   }}
-                  className="text-xs italic text-gray-500"
                 >
-                  <p className="pt-1">{entry.notes}</p>
-                </motion.div>
-              )}
-            </AnimatePresence>
-          </motion.div>
-        </div>
-        <span className="absolute inset-y-0 right-0 flex items-center p-2">
-          {!expanded ? (
-            <PlusIcon className="h-4 w-4" />
-          ) : (
-            <MinusIcon className="h-4 w-4" />
+                  <span>
+                    {set.weight} lbs – {pluralize("rep", set.reps, true)}
+                  </span>
+                  <AnimatePresence>
+                    {expanded && set.tracked && (
+                      <motion.span
+                        key={set.id}
+                        initial={{ opacity: 0 }}
+                        exit={{
+                          opacity: 0,
+                          transition: { type: "spring", duration: 0.4 },
+                        }}
+                        animate={{ opacity: 1 }}
+                      >
+                        {" "}
+                        👈
+                      </motion.span>
+                    )}
+                  </AnimatePresence>
+                </motion.p>
+              </motion.div>
+            ))}
+          {expanded && entry.notes && (
+            <motion.div
+              layout="position"
+              initial={{ opacity: 0, height: 0 }}
+              animate={{ opacity: 1, height: "auto" }}
+              exit={{
+                opacity: 0,
+                height: 0,
+                transition: { type: "spring", duration: 0.4 },
+              }}
+              className="text-xs italic text-gray-500"
+            >
+              <p className="pt-1">{entry.notes}</p>
+            </motion.div>
           )}
-        </span>
-      </button>
-    </div>
+        </AnimatePresence>
+      </motion.div> */}
+    </>
   );
 }
