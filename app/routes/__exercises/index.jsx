160fa832ffbc879729b0f34fa59d3968767ec169
Sam Selikoff <sam.selikoff@gmail.com>
2024-04-05 18:04:23 -0400

    add back animation

--- app/routes/__exercises/index.jsx
+++ app/routes/__exercises/index.jsx
@@ -14,6 +14,7 @@ import { prisma } from "~/db.server";
 import { requireUserId } from "~/session.server";
 import timeAgo from "~/utils/time-ago";
 import pluralize from "pluralize";
+import { AnimatePresence, motion } from "framer-motion";
 
 export async function loader({ request }) {
   let userId = await requireUserId(request);
@@ -47,7 +48,7 @@ export default function ExercisesIndexPage() {
           </Flex>
         </>
       ) : (
-        <p className="mt-6">Choose an exercise.</p>
+        <Text>Choose an exercise.</Text>
       )}
     </Box>
   );
@@ -68,8 +69,77 @@ function EntryCard({ entry }) {
               {timeAgo(entry.date)}
             </Text>
           </Flex>
+
           <Box>
-            {entry.sets
+            <motion.div layout="position" className="overflow-hidden">
+              <AnimatePresence initial={false}>
+                {entry.sets
+                  .filter((set) => (!expanded ? set.tracked : true))
+                  .map((set) => (
+                    <motion.div
+                      layout="position"
+                      variants={{
+                        hidden: { height: 0 },
+                        visible: { height: "auto" },
+                      }}
+                      initial="hidden"
+                      animate="visible"
+                      exit="hidden"
+                      key={set.id}
+                    >
+                      <motion.div
+                        variants={{
+                          hidden: {
+                            opacity: 0,
+                            transition: { type: "spring", duration: 0.35 },
+                          },
+                          visible: { opacity: 1 },
+                        }}
+                      >
+                        <Text>
+                          {set.weight} lbs – {pluralize("rep", set.reps, true)}
+                        </Text>
+                        <AnimatePresence>
+                          {expanded && set.tracked && (
+                            <motion.span
+                              key={set.id}
+                              initial={{ opacity: 0 }}
+                              exit={{
+                                opacity: 0,
+                                transition: { type: "spring", duration: 0.4 },
+                              }}
+                              animate={{ opacity: 1 }}
+                            >
+                              {" "}
+                              👈
+                            </motion.span>
+                          )}
+                        </AnimatePresence>
+                      </motion.div>
+                    </motion.div>
+                  ))}
+                {expanded && entry.notes && (
+                  <motion.div
+                    layout="position"
+                    initial={{ opacity: 0, height: 0 }}
+                    animate={{ opacity: 1, height: "auto" }}
+                    exit={{
+                      opacity: 0,
+                      height: 0,
+                      transition: { type: "spring", duration: 0.4 },
+                    }}
+                  >
+                    <Box pt="4">
+                      <Text size="2" color="gray">
+                        <Em>{entry.notes}</Em>
+                      </Text>
+                    </Box>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </motion.div>
+
+            {/* {entry.sets
               .filter((set) => (!expanded ? set.tracked : true))
               .map((set) => (
                 <Text as="p" key={set.id}>
@@ -81,77 +151,12 @@ function EntryCard({ entry }) {
               <Text color="gray" size="2">
                 <Em>{entry.notes}</Em>
               </Text>
-            )}
+            )} */}
           </Box>
         </button>
       </Reset>
 
       <Separator size="4" />
-
-      {/* <motion.div layout="position" className="overflow-hidden">
-        <AnimatePresence initial={false}>
-          {entry.sets
-            .filter((set) => (!expanded ? set.tracked : true))
-            .map((set) => (
-              <motion.div
-                layout="position"
-                variants={{
-                  hidden: { height: 0 },
-                  visible: { height: "auto" },
-                }}
-                initial="hidden"
-                animate="visible"
-                exit="hidden"
-                key={set.id}
-              >
-                <motion.p
-                  variants={{
-                    hidden: {
-                      opacity: 0,
-                      transition: { type: "spring", duration: 0.35 },
-                    },
-                    visible: { opacity: 1 },
-                  }}
-                >
-                  <span>
-                    {set.weight} lbs – {pluralize("rep", set.reps, true)}
-                  </span>
-                  <AnimatePresence>
-                    {expanded && set.tracked && (
-                      <motion.span
-                        key={set.id}
-                        initial={{ opacity: 0 }}
-                        exit={{
-                          opacity: 0,
-                          transition: { type: "spring", duration: 0.4 },
-                        }}
-                        animate={{ opacity: 1 }}
-                      >
-                        {" "}
-                        👈
-                      </motion.span>
-                    )}
-                  </AnimatePresence>
-                </motion.p>
-              </motion.div>
-            ))}
-          {expanded && entry.notes && (
-            <motion.div
-              layout="position"
-              initial={{ opacity: 0, height: 0 }}
-              animate={{ opacity: 1, height: "auto" }}
-              exit={{
-                opacity: 0,
-                height: 0,
-                transition: { type: "spring", duration: 0.4 },
-              }}
-              className="text-xs italic text-gray-500"
-            >
-              <p className="pt-1">{entry.notes}</p>
-            </motion.div>
-          )}
-        </AnimatePresence>
-      </motion.div> */}
     </>
   );
 }
